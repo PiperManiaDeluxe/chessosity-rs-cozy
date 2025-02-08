67a828a123b1f4128e922c876ef28e4384b3ea54
Piper <pipermaniadeluxe@gmail.com>
2025-02-07 18:59:23 -0700

    Forgot to make the option exist lol

--- src/uci/uci_command_uci.rs
+++ src/uci/uci_command_uci.rs
@@ -1,5 +1,7 @@
 ﻿pub fn do_uci_command_uci() {
     println!("id name Chessosity");
     println!("id author Piper Mania Deluxe");
+    println!();
+    println!("option name hash_size type spin default 16777216 min 1 max 67108864");
     println!("uciok");
 }
