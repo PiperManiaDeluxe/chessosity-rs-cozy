c4f9b2f71e6c568b4f3982ce192d98c250cd43ae
Piper <pipermaniadeluxe@gmail.com>
2025-02-07 20:04:16 -0700

    Remove mop-up and make PST worth 5 times more

--- src/eval/eval.rs
+++ src/eval/eval.rs
@@ -1,7 +1,6 @@
 ﻿use cozy_chess::{Board, Color};
 use crate::eval::eval_count_material::eval_count_material;
 use crate::eval::eval_is_mate::eval_is_mate;
-use crate::eval::eval_mop_up::eval_mop_up;
 use crate::eval::eval_pst::{eval_pst_end, eval_pst_opening};
 use crate::eval::game_phase::get_game_phase;
 
@@ -21,13 +20,12 @@ pub fn eval(board: &Board, distance_from_root: u8) -> i32 {
 
 pub fn eval_opening(board: &Board, distance_from_root: u8) -> i32 {
     let mut score = eval_count_material(board);
-    score += eval_pst_opening(board) / 10;
+    score += eval_pst_opening(board) / 2;
     score
 }
 
 pub fn eval_endgame(board: &Board, distance_from_root: u8) -> i32 {
     let mut score = eval_count_material(board);
-    score += eval_pst_end(board) / 10;
-    score += eval_mop_up(board, distance_from_root);
+    score += eval_pst_end(board) / 2;
     score
 }
